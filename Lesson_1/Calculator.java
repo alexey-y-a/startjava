0404b482624e4682b8ac09704f49fd97efbe375c
alexey-y-a <lexus2157@mail.ru>
2025-01-13 19:52:59 +0300

    Исправлен код учитывая Guard Clauses

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -5,35 +5,30 @@ public class Calculator {
 
         int num1 = 2;
         int num2 = 10;
-        int result = 0;
         char operator = '^';
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else { 
-                System.out.println("Ошибка: деление на ноль");
-                return;
-            }
-        } else if (operator == '^') {
-            result = 1;
-            for (int i = 0; i < num2; i++) {
-                result *= num1;
-            }
-        } else if (operator == '%') {
-            if (num2 != 0) {
-                result = num1 % num2;
-            } else {
-                System.out.println("Ошибка: деление на ноль");
-                return;
-            }
-        } else {
+        if (operator != '+' && operator != '-' && operator != '*' && 
+                operator != '/' && operator != '^' && operator != '%') {
             System.out.println("Ошибка: неподдерживаемая операция");
+            return;
+        }
+        if ((operator == '/' || operator == '%') && num2 == 0) {
+            System.out.println("Ошибка: деление на ноль");
+            return;
+        }
+        int result = 0;
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> result = num1 / num2;
+            case '^' -> {
+                result = 1;
+                for (int i = 0; i < num2; i++) {
+                    result *= num1;
+                }
+            }
+            case '%' -> result = num1 % num2;
+            default -> System.out.println("Ошибка: неподдерживаемая операция");
         }
         System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
     }

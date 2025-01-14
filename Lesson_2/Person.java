d714e4f31a89af1f1be71f61a489640cd7ce8be9
alexey-y-a <lexus2157@mail.ru>
2025-01-15 00:50:02 +0300

    Создан class Person с полями и методами

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    
+    String gender = "m";
+    String name = "Alex";
+    int height = 170;
+    int age = 35;
+
+    void go() {
+        System.out.println("гуляет");
+    }
+
+    void sit() {
+        System.out.println("сидит");
+    }
+
+    void run() {
+        System.out.println("бежит");
+    }
+
+    void speak() {
+        System.out.println("говорит");
+    }
+
+    void learnJava() {
+        System.out.println("изучает Джава");
+    }
+}

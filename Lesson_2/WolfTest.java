1c85950cc71a3228a48cdd546cf16fd15b762c0c
alexey-y-a <lexus2157@mail.ru>
2025-01-17 01:47:51 +0300

    Модифицирован класс WolfTest

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,20 +2,28 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = "m";
-        wolf.nickName = "Alfa";
-        wolf.weight = 30;
-        wolf.age = 10;
-        wolf.color = "white";
-        System.out.println("Пол = " + wolf.gender);
-        System.out.println("Кличка = " + wolf.nickName);
-        System.out.println("Вес = " + wolf.weight);
-        System.out.println("Возраст = " + wolf.age);
-        System.out.println("Цвет = " + wolf.color);
+        wolf.setGender("m");
+        wolf.setNickName("Alfa");
+        wolf.setWeight(30);
+        wolf.setAge(10);
+        wolf.setColor("white");
+        System.out.println("Пол = " + wolf.getGender());
+        System.out.println("Кличка = " + wolf.getNickName());
+        System.out.println("Вес = " + wolf.getWeight());
+        System.out.println("Возраст = " + wolf.getAge());
+        System.out.println("Цвет = " + wolf.getColor());
         wolf.go();
         wolf.sit();
         wolf.run();
         wolf.howl();
         wolf.hunt();
+        wolf.setNickName("Beta");
+        wolf.setWeight(40);
+        wolf.setAge(15);
+        wolf.setColor("Black");
+        System.out.println("Кличка = " + wolf.getNickName());
+        System.out.println("Вес = " + wolf.getWeight());
+        System.out.println("Возраст = " + wolf.getAge());
+        System.out.println("Цвет = " + wolf.getColor());
     }
 }

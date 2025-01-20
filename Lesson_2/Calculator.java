ca4f0351c2c6bb1026cb010a73c17da0c1a009d0
alexey-y-a <lexus2157@mail.ru>
2025-01-20 18:07:05 +0300

    исправлены замечания в Calculator и CalculatorTest

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -2,16 +2,15 @@ public class Calculator {
 
     public static int calculate(int num1, char operator, int num2) {
         int result = 0;
+        if ((operator == '/' || operator == '%') && isDivisionZero(num2)) {
+            System.out.println("Ошибка: деление на ноль");
+            return Integer.MIN_VALUE;
+        }
         switch (operator) {
             case '+' -> result = num1 + num2;
             case '-' -> result = num1 - num2;
             case '*' -> result = num1 * num2;
-            case '/' -> {
-                if (num2 == 0) {
-                    return Integer.MIN_VALUE;
-                }
-                result = num1 / num2;
-            }
+            case '/' -> result = num1 / num2;
             case '^' -> {
                 result = 1;
                 int power = Math.abs(num2);
@@ -22,16 +21,16 @@ public class Calculator {
                     result = 1 / result;
                 }
             }
-            case '%' -> {
-                if (num2 == 0) {
-                    return Integer.MIN_VALUE;
-                }
-                result = num1 % num2;
-            }
+            case '%' -> result = num1 % num2;
             default -> {
+                System.out.println("Ошибка: операция '" + operator + "' не поддерживается");
                 return Integer.MIN_VALUE;
             }
         }
         return result;
+    }
+    
+    private static boolean isDivisionZero(int num) {
+        return num == 0;
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,23 +4,37 @@ public class CalculatorTest {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Введите выражение (например, 2 + 3): ");
-            int num1 = scanner.nextInt();
+        String response;
+        do {
+            int num1;
+            System.out.println("Введите первое число:");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Ошибка: введите целое число");
+                System.out.print("Введите первое число: ");
+                scanner.next();
+            }
+            num1 = scanner.nextInt();
+            System.out.println("Введите знак операции (+, -, *, /, ^, %): ");
             char operator = scanner.next().charAt(0);
+            System.out.print("Введите второе число: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Ошибка: введите целое число");
+                System.out.print("Введите второе число: ");
+                scanner.next();
+            }
             int num2 = scanner.nextInt();
             int result = Calculator.calculate(num1, operator, num2);
-            if (result == Integer.MIN_VALUE) {
-                System.out.println("Ошибка: некорректная операция или деление на ноль");
-            } else {
+            if (result != Integer.MIN_VALUE) {
                 System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
-            }
-            System.out.print("Хотите продолжить вычисления? [yes/no]: ");
-            String response = scanner.next().toLowerCase();
-            if (response.equals("no")) {
-                break;
-            }
-        }
+            } 
+            do {
+                System.out.print("Хотите продолжить вычисления? [yes/no]: ");
+                response = scanner.next().toLowerCase();
+                if (!response.equals("yes") && !response.equals("no")) {
+                    System.out.println("Ошибка: введите 'yes' или 'no'");
+                }
+            } while (!response.equals("yes") && !response.equals("no"));
+        } while (response.equals("yes"));
         System.out.println("Программа завершена");
         scanner.close();
     }

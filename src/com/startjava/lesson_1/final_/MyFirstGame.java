d420077e8240315df560df645781f869cafa6d59
alexey-y-a <lexus2157@mail.ru>
2025-01-28 01:28:54 +0300

    Переименовал папку final в final_ и обновил файлы

---
+++ src/com/startjava/lesson_1/final_/MyFirstGame.java
@@ -0,0 +1,21 @@
+package com.startjava.lesson_1.final_;
+
+public class MyFirstGame {
+    
+    public static void main(String[] args) {
+        System.out.println("\nИгра Угадай число\n");
+
+        int secretNumber = 50;
+        System.out.println("\nКомпьютер загадал число от 1 до 100. Попробуйте угадать!\n");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Игрок выбрал число: " + i);
+            if (i == secretNumber) {
+                System.out.println("Вы победили!");
+                break;
+            }
+            if (i > secretNumber) {
+                System.out.println(i + " ,больше того, что загадал компьютер");
+            }
+        }
+    }
+}

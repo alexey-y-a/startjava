fbffca6d7dbde4395853c61240849d170cf79025
alexey-y-a <lexus2157@mail.ru>
2025-02-03 19:33:55 +0300

    в методе main реализован вызов двух разных методаов

--- src/com/startjava/lesson_2_3_4/array/Reverser.java
+++ src/com/startjava/lesson_2_3_4/array/Reverser.java
@@ -6,39 +6,41 @@ public class Reverser {
 
     public static void main(String[] args) {
         int[] emptyArray = new int[0];
-        reversePrint(emptyArray);
+        reverseArray(emptyArray);
+        printArray(emptyArray);
 
         int[] nullArray = null;
-        reversePrint(nullArray);
+        reverseArray(nullArray);
+        printArray(nullArray);
 
         int[] array1 = {6, 8, 9, 1};
-        reversePrint(array1);
+        reverseArray(array1);
+        printArray(array1);
 
         int[] array2 = {13, 8, 5, 3, 2, 1, 1};
-        reversePrint(array2);
-    }
-
-    private static void reversePrint(int[] array) {
-        if (array == null) {
-            System.out.println("Массив является null.");
-            return;
-        }
-        if (array.length == 0) {
-            System.out.println("Массив пустой.");
-            return;
-        }
-
-        System.out.println("\nДо реверса: " + Arrays.toString(array));
-        reverseArray(array);
-        System.out.println("После реверса: " + Arrays.toString(array));
+        reverseArray(array2);
+        printArray(array2);
     }
 
     private static void reverseArray(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
         int length = array.length;
         for (int i = 0; i < length / 2; i++) {
             int tmp = array[i];
             array[i] = array[length - 1 - i];
             array[length - 1 - i] = tmp;
+        }
+    }
+
+    private static void printArray(int[] array) {
+        if (array == null) {
+            System.out.println("Массив является null.");
+        } else if (array.length == 0) {
+            System.out.println("Массив пустой");
+        } else {
+            System.out.println("После реверса: " + Arrays.toString(array));
         }
     }
 }

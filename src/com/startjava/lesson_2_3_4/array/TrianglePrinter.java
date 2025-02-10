fdde9d76ea97187b4db8d26811daeafd94300cee
alexey-y-a <lexus2157@mail.ru>
2025-02-10 15:11:10 +0300

    Объединены строки, при возникновении ошибки треугольник не выводится

--- src/com/startjava/lesson_2_3_4/array/TrianglePrinter.java
+++ src/com/startjava/lesson_2_3_4/array/TrianglePrinter.java
@@ -3,50 +3,44 @@ package com.startjava.lesson_2_3_4.array;
 public class TrianglePrinter {
     public static void main(String[] args) {
         TrianglePrinter printer = new TrianglePrinter();
-        char[] sortedChars;
 
-        sortedChars = printer.generateSortedChars('0', '9', true);
-        printer.printTriangle(sortedChars);
+        char[]sortedChars = printer.generateSortedChars('0', '9', true);
+        if (sortedChars != null) {
+            printer.printTriangle(sortedChars);
+        }
 
         sortedChars = printer.generateSortedChars('/', '!', false);
-        printer.printTriangle(sortedChars);
+        if (sortedChars != null) {
+            printer.printTriangle(sortedChars);
+        }
 
         sortedChars = printer.generateSortedChars('A', 'J', false);
-        printer.printTriangle(sortedChars);
+        if (sortedChars != null) {
+            printer.printTriangle(sortedChars);
+        }
     }
 
     private char[] generateSortedChars(char start, char end, boolean ascending) {
         if (start > end) {
             System.out.printf("Ошибка: левая граница (%d) > правой (%d)%n", (int) start, (int) end);
-            char temp = start;
-            start = end;
-            end = temp;
+            return null;
         }
 
         char[] chars = new char[end - start + 1];
         for (int i = 0; i < chars.length; i++) {
-            chars[i] = (char) (start + i);
-        }
-
-        if (!ascending) {
-            int length = chars.length;
-            for (int i = 0; i < length; i++) {
-                char temp = chars[i];
-                chars[i] = chars[--length];
-                chars[length] = temp;
-            }
+            chars[i] = (char) (ascending ? start + i : end - i);
         }
         return chars;
     }
 
     private void printTriangle(char[] chars) {
         int maxWidth = chars.length * 2 - 1;
-        StringBuilder sb = new StringBuilder();
+        StringBuilder triangle = new StringBuilder();
 
         for (int i = 0; i < chars.length; i++) {
             String line = String.valueOf(chars[i]).repeat(i * 2 + 1);
-            sb.append(" ".repeat((maxWidth - line.length()) / 2)).append(line).append("\n");
+            triangle.append(" ".repeat((maxWidth - line.length()) / 2)).append(line).append("\n");
         }
-        System.out.print(sb);
+        System.out.print(triangle);
     }
 }

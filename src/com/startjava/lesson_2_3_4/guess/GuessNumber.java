83d8865b6c127a50e869e921537ecbf99742c2d9
alexey-y-a <lexus2157@mail.ru>
2025-02-19 22:43:11 +0300

    ДЗ модификация "Угадай число"

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,9 +1,11 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GuessNumber {
-    
+
+    private static final int MAX_ATTEMPTS = 10;
     private final Player player1;
     private final Player player2;
     private final int secretNumber;
@@ -15,47 +17,51 @@ public class GuessNumber {
     }
 
     public void start() {
-        System.out.println("Компьютер загадал число от 1 до 100. Угадай число!");
+        System.out.println("Игра началась! У каждого игрока по " + MAX_ATTEMPTS + " попыток");
         Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
+            System.out.println("Попытка " + i);
+            if (makeGuess(player1, scanner, i) || makeGuess(player1, scanner, i)) {
+                displayResults();
+                return;
+            }
+        }
+
+        System.out.println("у " + player1.getName() + " закончились попытки!");
+        System.out.println("у " + player2.getName() + " закончились попытки!");
+        displayResults();
+    }
+
+    private boolean makeGuess(Player player, Scanner scanner, int attempt) {
         while (true) {
-            if (makeGuess(player1, scanner) || makeGuess(player2, scanner)) {
+            try {
+                System.out.println("Число вводит " + player.getName() + ": ");
+                int number = Integer.parseInt(scanner.nextLine());
+                player.addNumber(number);
+                String message = number == secretNumber ? "равно" :
+                        (number < secretNumber ? "меньше" : "больше");
+                System.out.println("Число " + number + " " + message + " загаданного");
+                if (number == secretNumber) {
+                    System.out.println(player.getName() + " угадал число " +
+                            secretNumber + " с " + attempt + "-й попытки!");
+                    return true;
+                }
                 break;
-            }
-        }
-    }
-
-    private boolean makeGuess(Player player, Scanner scanner) {
-        int number = inputNumber(player.getName(), scanner);
-        player.setNumber(number);
-        return checkNumber(player);
-    }
-
-    private int inputNumber(String playerName, Scanner scanner) {
-        while (true) {
-            System.out.print(playerName + ", введите число: ");
-            if (scanner.hasNextInt()) {
-                return scanner.nextInt();
-            } else {
+            } catch (NumberFormatException e) {
                 System.out.println("Ошибка: введите целое число!");
-                scanner.next();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage() + " Попробуйте еще раз:");
             }
         }
+        return false;
     }
 
-    private boolean checkNumber(Player player) {
-        int number = player.getNumber();
-        
-        if (number == secretNumber) {
-            System.out.println("Поздравляем, " + player.getName() + "! Вы угадали число!");
-            return true;
-        }
-        
-        if (number < secretNumber) {
-            System.out.println("Число " + number + " меньше загаданного");
-        } else {
-            System.out.println("Число " + number + " больше загаданного");
-        }
-        
-        return false;
+    private void displayResults() {
+        System.out.println("Числа игрока " + player1.getName() + ": " + Arrays.toString(player1.getNumbers()));
+        System.out.println("Числа игрока " + player2.getName() + ": " + Arrays.toString(player2.getNumbers()));
+
+        player1.clearNumbers();
+        player2.clearNumbers();
     }
 }
--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,23 +1,42 @@
 package com.startjava.lesson_2_3_4.guess;
 
+import java.util.Arrays;
+
 public class Player {
-    
-    private String name;
-    private int number;
+
+    private static final int MAX_ATTEMPTS = 10;
+    private final String name;
+    private final int[] numbers = new int[MAX_ATTEMPTS];
+    private int attemptCount;
 
     public Player(String name) {
         this.name = name;
+        this.attemptCount = 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getNumber() {
-        return number;
+    public void addNumber(int number) throws IllegalArgumentException {
+        if (number < 1 || number > 100) {
+            throw new IllegalArgumentException("Число должно входить в отрезок [1, 100]");
+        }
+        if (attemptCount < MAX_ATTEMPTS) {
+            numbers[attemptCount++] = number;
+        }
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public int[] getNumbers() {
+        return Arrays.copyOf(numbers, attemptCount);
+    }
+
+    public void clearNumbers() {
+        Arrays.fill(numbers, 0, attemptCount, 0);
+        attemptCount = 0;
+    }
+
+    public int getAttemptCount() {
+        return attemptCount;
     }
 }

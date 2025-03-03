b58663e75fe33d13715fc3fc29162673c511ec0a
alexey-y-a <lexus2157@mail.ru>
2025-03-03 12:43:45 +0300

    Выполнено форматирование кода согласно Руководству SQL

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,23 +1,18 @@
 \c robots;
 
-
 DROP TABLE IF EXISTS jaegers;
 
-
 CREATE TABLE jaegers (
-    id SERIAL PRIMARY KEY,
-    model_name VARCHAR(50) NOT NULL,
-    mark VARCHAR(50) NOT NULL,
-    height NUMERIC(5, 2) NOT NULL,
-    weight NUMERIC(6, 2) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    origin VARCHAR(50) NOT NULL,
-    launch DATE NOT NULL,
-    kaiju_kill INT NOT NULL
+    id          SERIAL        PRIMARY KEY,
+    model_name  VARCHAR(50)   NOT NULL,
+    mark        VARCHAR(50)   NOT NULL,
+    height      NUMERIC(5, 2) NOT NULL,
+    weight      NUMERIC(6, 2) NOT NULL,
+    status      VARCHAR(20)   NOT NULL,
+    origin      VARCHAR(50)   NOT NULL,
+    launch      DATE          NOT NULL,
+    kaiju_kill  INT           NOT NULL
 );
 
-
 \i populate.sql
-
-
 \i queries.sql
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,31 +1,52 @@
-SELECT * FROM jaegers ORDER BY model_name ASC;
+-- Вывод всех роботов в порядке возрастания по model_name
+SELECT *
+  FROM jaegers
+ ORDER BY model_name ASC;
 
+-- Вывод всех роботов, которые не уничтожены
+SELECT *
+  FROM jaegers
+ WHERE status != 'Destroyed';
 
-SELECT * FROM jaegers WHERE status != 'Destroyed';
+-- Вывод всех роботов серий Mark-1 и Mark-4
+SELECT *
+  FROM jaegers
+ WHERE mark IN ('Mark-1', 'Mark-4');
 
+-- Вывод всех роботов, кроме Mark-1 и Mark-4, в порядке убывания по mark
+SELECT *
+  FROM jaegers
+ WHERE mark NOT IN ('Mark-1', 'Mark-4')
+ ORDER BY mark DESC;
 
-SELECT * FROM jaegers WHERE mark IN ('Mark-1', 'Mark-4');
+-- Вывод самого старого робота
+SELECT *
+  FROM jaegers
+ ORDER BY launch ASC
+ LIMIT 1;
 
+-- Вывод роботов, которые уничтожили больше всех Kaiju
+SELECT model_name, mark, launch, kaiju_kill
+  FROM jaegers
+ WHERE kaiju_kill = (SELECT MAX(kaiju_kill) FROM jaegers);
 
-SELECT * FROM jaegers WHERE mark NOT IN ('Mark-1', 'Mark-4') ORDER BY mark DESC;
+-- Вывод среднего веса роботов
+SELECT ROUND(AVG(weight), 3) AS avg_weight
+  FROM jaegers;
 
+-- Увеличение количества убитых Kaiju на 1 для всех активных роботов
+UPDATE jaegers
+   SET kaiju_kill = kaiju_kill + 1
+ WHERE status != 'Destroyed';
 
-SELECT * FROM jaegers ORDER BY launch ASC LIMIT 1;
+-- Вывод всех роботов после обновления
+SELECT *
+  FROM jaegers;
 
+-- Удаление уничтоженных роботов
+DELETE FROM jaegers
+ WHERE status = 'Destroyed';
 
-SELECT model_name, mark, launch, kaiju_kill FROM jaegers ORDER BY kaiju_kill DESC LIMIT 1;
-
-
-SELECT ROUND(AVG(weight), 3) AS avg_weight FROM jaegers;
-
-
-UPDATE jaegers SET kaiju_kill = kaiju_kill + 1 WHERE status != 'Destroyed';
-
-
-SELECT * FROM jaegers;
-
-
-DELETE FROM jaegers WHERE status = 'Destroyed';
-
-
-SELECT * FROM jaegers;
+-- Вывод всех роботов после удаления
+SELECT *
+  FROM jaegers;
